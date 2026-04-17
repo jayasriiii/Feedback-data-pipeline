0ae128ce3ea5926f791502a063193c80ea49dfda
Jayasri <120169052+jayasriiii@users.noreply.github.com>
2026-04-17 15:54:57 +0530

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,17 +1,11 @@
 from ingest import read_data
-from validate import validate_data
-from transform import transform_data
+from transform import process_data
+from load import save_data
 
 def run_pipeline():
-    data = read_data("data/raw/input.json")
-
-    data = validate_data(data)
-
-    data = transform_data(data)
-
-    import json
-    with open("data/processed/output.json", "w") as f:
-        json.dump(data, f, indent=4)
+    data = read_data()
+    processed_data = process_data(data)
+    save_data(processed_data)
 
 if __name__ == "__main__":
     run_pipeline()
