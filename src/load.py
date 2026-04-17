693c65dd6f7b3626bfdf63356cb71ba327007e00
Jayasri <120169052+jayasriiii@users.noreply.github.com>
2026-04-17 15:55:24 +0530

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,9 +1,10 @@
+def save_data(df):
+    if df is None:
+        print("❌ No data to save")
+        return
 
-def validate_data(data):
-    valid_data = []
-
-    for record in data:
-        if "feedback" in record and record["feedback"]:
-            valid_data.append(record)
-
-    return valid_data
+    try:
+        df.to_csv("data/processed_feedback.csv", index=False)
+        print("✅ Data saved successfully")
+    except Exception as e:
+        print("❌ Error saving data:", e)
