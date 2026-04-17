c6d6d7a7b90d266934100315d7c02f7652bc7c74
Jayasri <120169052+jayasriiii@users.noreply.github.com>
2026-04-17 14:12:48 +0530

    Update validate.py

--- src/validate.py
+++ src/validate.py
@@ -1,1 +1,9 @@
 
+def validate_data(data):
+    valid_data = []
+
+    for record in data:
+        if "feedback" in record and record["feedback"]:
+            valid_data.append(record)
+
+    return valid_data
